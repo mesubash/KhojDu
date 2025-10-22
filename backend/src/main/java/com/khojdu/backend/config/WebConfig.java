bea6952dd237735857020707857f855ad11a6ce0
Subash Singh Dhami <subashdhamee@gmail.com>
2025-10-22 14:46:04 +0545

    add web config file

---
+++ backend/src/main/java/com/khojdu/backend/config/WebConfig.java
@@ -0,0 +1,29 @@
+package com.khojdu.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        PageableHandlerMethodArgumentResolver pageableResolver = new PageableHandlerMethodArgumentResolver();
+        pageableResolver.setOneIndexedParameters(true);
+        pageableResolver.setMaxPageSize(100);
+        pageableResolver.setFallbackPageable(PageRequest.of(0, 20));
+        resolvers.add(pageableResolver);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:uploads/");
+    }
+}
