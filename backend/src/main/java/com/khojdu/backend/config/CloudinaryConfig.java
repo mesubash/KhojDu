b5794375114512576325ac7db4bfc0fb5127ff59
Subash Singh Dhami <subashdhamee@gmail.com>
2025-10-22 14:47:02 +0545

    add cloudinary config

---
+++ backend/src/main/java/com/khojdu/backend/config/CloudinaryConfig.java
@@ -0,0 +1,30 @@
+package com.khojdu.backend.config;
+
+import com.cloudinary.Cloudinary;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "file.cloudinary")
+public class CloudinaryConfig {
+
+    private String cloudName;
+    private String apiKey;
+    private String apiSecret;
+
+    @Bean
+    public Cloudinary cloudinary() {
+        Map<String, String> config = new HashMap<>();
+        config.put("cloud_name", cloudName);
+        config.put("api_key", apiKey);
+        config.put("api_secret", apiSecret);
+        config.put("secure", "true");
+        return new Cloudinary(config);
+    }
+}
